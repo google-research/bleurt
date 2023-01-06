fbad0131a3bd9a24a4d613f57cf848b08c81fcf7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-06 20:29:40 +0000

    Adding tarfile member sanitization to extractall()

--- bleurt/wmt/downloaders.py
+++ bleurt/wmt/downloaders.py
@@ -308,7 +308,26 @@ class Importer17(WMTImporter):
     segments_archive = os.path.join(package_path, "input",
                                     "wmt17-metrics-task-no-hybrids.tgz")
     with (tarfile.open(segments_archive, "r:gz")) as tar:
-      tar.extractall(path=self.segments_path())
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner) 
+          
+      
+      safe_extract(tar, path=self.segments_path())
     logging.info("Unpacked the segments to {}.".format(self.segments_path()))
 
     # Gets the language pair names.
